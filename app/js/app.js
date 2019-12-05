563dc89255ea2a283ded58c899b096437907909d
DanyDelattaignant <delattaignant.dany@gmail.com>
2019-12-05 19:21:44 +0100

    maj index pour mettre en place scroll magic

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,71 @@
 var app = {
-    init: function(
-        
-    ) {
-      console.log('init');
-    }
-  };
+  init: function() {
+
+var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({
+
+    triggerElement: '.box1',
+    reverse: false
+
+
+})
+
+    .setClassToggle('.box1', 'fade-in')
+    .addIndicators({
+        name: 'INDICATIONS',
+        indent: 200,
+        colorStart: '#fff'
+    })
+    .addTo(controller);
+
+
+var scene2 = new ScrollMagic.Scene({
+
+    triggerElement: '.box2',
+    reverse: false
+
+
+})
+
+    .setClassToggle('.box2', 'fade-in')
+    .addIndicators({
+        name: 'INDICATIONS',
+        indent: 200,
+        colorStart: '#fff'
+    })
+    .addTo(controller);
+
+var scene3 = new ScrollMagic.Scene({
+
+    triggerElement: '.box3',
+    reverse: false
+
+
+})
+
+    .setClassToggle('.box3', 'fade-in')
+    .addIndicators({
+        name: 'INDICATIONS',
+        indent: 200,
+        colorStart: '#fff'
+    })
+    .addTo(controller);
+
+
+
+ var app = {
+  init: function() {
+    console.log('init');
+  }
+};
+
+$(app.init);
+
+    console.log('init');
+  }
+
   
- 
+};
 
-  $(app.init);
-
+$(app.init);
