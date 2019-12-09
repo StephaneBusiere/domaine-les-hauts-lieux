f89c4eab540823dc58f73740aeb071c6395684cd
DanyDelattaignant <delattaignant.dany@gmail.com>
2019-12-09 10:40:51 +0100

    ajout modal header + correction mineures scroll magic

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,7 @@
 var app = {
   init: function() {
 
+    // scroll magic 
 var controller = new ScrollMagic.Controller();
 
 var scene = new ScrollMagic.Scene({
@@ -8,50 +9,26 @@ var scene = new ScrollMagic.Scene({
     triggerElement: '.box1',
     reverse: false
 
-
 })
-
-    .setClassToggle('.box1', 'fade-in')
-    .addIndicators({
-        name: 'INDICATIONS',
-        indent: 200,
-        colorStart: '#fff'
-    })
+    .setClassToggle('.box1', 'fade-in')   
     .addTo(controller);
-
 
 var scene2 = new ScrollMagic.Scene({
 
     triggerElement: '.box2',
     reverse: false
-
-
 })
-
-    .setClassToggle('.box2', 'fade-in')
-    .addIndicators({
-        name: 'INDICATIONS',
-        indent: 200,
-        colorStart: '#fff'
-    })
+    .setClassToggle('.box2', 'fade-in')  
     .addTo(controller);
 
 var scene3 = new ScrollMagic.Scene({
 
     triggerElement: '.box3',
     reverse: false
-
-
 })
-
     .setClassToggle('.box3', 'fade-in')
-    .addIndicators({
-        name: 'INDICATIONS',
-        indent: 200,
-        colorStart: '#fff'
-    })
-    .addTo(controller);
-
+    .addTo(controller); 
+    // fin scroll magic
 
 
  var app = {
@@ -65,7 +42,16 @@ $(app.init);
     console.log('init');
   }
 
-  
 };
 
 $(app.init);
+// Le code suivant va fermer la modal en cliquant en dehors du contenu modal '(et pas seulement en utilisant "x" ou "annuler'")
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
