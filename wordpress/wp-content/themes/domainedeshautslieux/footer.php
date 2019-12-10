ef1e0a3695717d1aa2f7985668dbb708c9cb3ba4
Nsahed <nacerasahed@hotmail.com>
2019-12-10 08:23:49 +0100

    creation footer.php

---
+++ wordpress/wp-content/themes/domainedeshautslieux/footer.php
@@ -0,0 +1,63 @@
+<!-- Layout : Footer -->
+<footer class="footer">
+        
+        <div class="footer__contact-informations">
+            <!-- Component : Contact information * 3 --->
+                    <h4>À propos </h4>
+            <div>
+                <h5 class="contact-information__title">E-mail</h5>
+                <a class="contact-information__text" href="mailto:ledomaine@gmail.com">ledomaine@gmail.com</a>
+            </div>
+            <div>
+                <h5 class="contact-information__title">Téléphone</h5>
+                <a class="contact-information__text" href="tel:+33607080910">+33 1 23 45 67 89</a>
+            </div>
+   
+            <div class="contact-information">
+                <h5 class="contact-information__title">Adresse</h5>
+                <p class="contact-information__text">Lorem ipsum dolor sit amet. <br />Lorem</p>
+            </div>
+      
+
+            <div class="question">
+                <h4> Des questions ?</h4>
+                <p>domainedeshautslieux@gmail.com</p>
+            </div>
+
+            <div class="question">
+                <h4> Latest Events</h4>
+                <ul class="recent-events">
+                        <li>	
+                            <label> 2 <br><span>DEC</span></label>
+                            <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, sit voluptas soluta ducimus qui eaque.</span></li>
+                        <li>
+                            <label> 23 <br><span>DEC</span></label>
+                            <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, sit voluptas soluta ducimus qui eaque.</span></li>
+                        <li>
+                            <label> 2 <br><span>JAN</span></label>
+                            <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, sit voluptas soluta ducimus qui eaque.</span></li>
+                </ul>
+            </div>
+    <div>
+        <div>
+            <h4>Newsletters</h4>
+            <div>
+            <input type="text" class="form-control" placeholder="Your Email Address">
+            </div>
+       
+            <nav class="menu menu--social">
+            <ul class="menu__list">
+                <li class="menu__list__item"><a href="#" <i class="fa fa-facebook"></i></a></li>
+                <li class="menu__list__item"><a href="#" <i class="fa fa-twitter"></i></a></li>
+                <li class="menu__list__item"><a href="#" <i class="fa fa-instagram"></i></a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
+</div>
+  </footer>
+
+<script src="js/app.js"></script>
+<?php wp_footer(); ?>
+</body>
+</html>
