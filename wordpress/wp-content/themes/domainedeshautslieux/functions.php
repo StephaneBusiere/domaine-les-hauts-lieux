5eb9928fc4d21462795ea9556606e49ba62fee4d
Nsahed <nacerasahed@hotmail.com>
2019-12-10 08:19:34 +0100

    add function.php for title tag and post-thumbnails

---
+++ wordpress/wp-content/themes/domainedeshautslieux/functions.php
@@ -0,0 +1,9 @@
+<?php 
+
+//Ajouter la prise en charge des images mises en avant
+
+add_theme_support( 'post-thumbnails' );
+
+//Ajouter automatiqument le titre du site d'en l'en-tête du site
+
+add_theme_support( 'title-tag');
